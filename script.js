3d8652afaf9af1ed0abb4cbee1ce1a8e4efb57bf
Md Sajid <sajidcse202024@gmail.com>
2026-06-29 00:43:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	 for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " ") {
+      return str[i];
+    }
+  }
+  return "";
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
 //const text = prompt("Enter text:");
+const str = prompt("Enter a string:");
 alert(firstChar(text));
