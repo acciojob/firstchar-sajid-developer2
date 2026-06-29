bb3cede76b76020a0fa8caf3c72a0c873d406e07
Md Sajid <sajidcse202024@gmail.com>
2026-06-29 17:41:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstChar(text) {
-  return text.trim().charAt(0);
+  if (text === "") {
+    return "";
+  }
+
+  let chars = text.split("");
+
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] !== " ") {
+      return chars[i];
+    }
+  }
+
+  return "";
 }
 
 // Do not change the code below
